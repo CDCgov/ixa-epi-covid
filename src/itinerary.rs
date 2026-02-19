6033ba42b7c30b4f41215b60a1dee33ed885da1b
Erik Rosenstrom <xui6@cdc.gov>
2026-02-19 11:34:29 -0800

    work in progress

--- src/itinerary.rs
+++ src/itinerary.rs
@@ -3,12 +3,12 @@ use ixa::{HashMap, csv, prelude::*};
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 
-use crate::{parameters::{ContextParametersExt, Params}, population_loader::PersonId};
+use crate::{parameters::{ContextParametersExt, Params}, population_loader::{PersonId}};
 
 use ixa::profiling::open_span;
 
 define_entity!(Itinerary);
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Serialize, Hash)]
 struct PersonItinerary(PersonId);
-define_property!(PersonItinerary);
+impl_property!(PersonItinerary, Itinerary);
