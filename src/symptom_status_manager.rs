71100f711aadff95a92fedef8e7d727ee321e5dd
Eric Q. Mooring <pgv5@cdc.gov>
2026-02-28 08:05:30 +0000

    initial commit symptom status manager

---
+++ src/symptom_status_manager.rs
@@ -0,0 +1,89 @@
+use ixa::{Context, ContextEntitiesExt, ContextRandomExt, IxaError, define_rng, impl_property, prelude::PropertyChangeEvent};
+use rand_distr::{LogNormal};
+use serde::{Deserialize, Serialize};
+
+use crate::{
+    infectiousness_manager::InfectionStatus, population_loader::Person
+};
+
+define_rng!(SymptomsRng);
+
+#[derive(Serialize, Deserialize, PartialEq, Debug, Copy, Clone)]
+pub enum SymptomStatus {
+    NoSymptoms,
+    Mild,
+    Severe,
+    Critical,
+    Resolved,
+    Dead
+}
+
+impl_property!(
+    SymptomStatus,
+    Person,
+    default_const = SymptomStatus::NoSymptoms
+);
+
+pub fn init(context: &mut Context) -> Result<(), IxaError> {
+    context.subscribe_to_event(
+        move|context, event: PropertyChangeEvent<Person, InfectionStatus>|{
+            if event.current == InfectionStatus::Infectious {
+                if context.sample_bool(SymptomsRng, probability_mild_given_infect){
+                    let infect_to_mild = LogNormal::new(infect_to_mild_mu, infect_to_mild_sigma).unwrap();
+                    let mild_time = context.get_current_time() + context.sample_distr(SymptomsRng,   infect_to_mild);
+                    context.add_plan(mild_time, move|context|{
+                        context.set_property(event.entity_id,SymptomStatus::Mild);
+                    });
+                    if context.sample_bool(SymptomsRng, probability_severe_given_mild){
+                        let mild_to_severe = LogNormal::new(mild_to_severe_mu, mild_to_severe_sigma).unwrap();
+                        let severe_time = mild_time + context.sample_distr(SymptomsRng, mild_to_severe);
+                        context.add_plan(severe_time, |context|{
+                            context.set_property(event.entity_id, SymptomStatus::Severe);
+                        });
+                        if context.sample_bool(SymptomsRng, probability_critical_given_severe){
+                            let severe_to_critical = LogNormal::new(severe_to_critical_mu, severe_to_critical_sigma).unwrap();
+                            let critical_time = severe_time + context.sample_distr(SymptomsRng, severe_to_critical);
+                            context.add_plan(critical_time, |context|{
+                                context.set_property(event.entity_id, SymptomStatus::Critical);
+                            });
+                            if context.sample_bool(SymptomsRng, probability_dead_given_critical){
+                                let critical_to_dead = LogNormal::new(critical_to_dead_mu, critical_to_dead_sigma).unwrap();
+                                let dead_time = critical_time + context.sample_distr(SymptomsRng, critical_to_dead);
+                                context.add_plan(dead_time, |context|{
+                                    context.set_property(event.entity_id, SymptomStatus::Dead);
+                                });
+                            } else {
+                                let critical_to_resolved = LogNormal::new(critical_to_resolved_mu, critical_to_resolved_sigma).unwrap();
+                                let resolved_time = critical_time + context.sample_distr(SymptomsRng, critical_to_resolved);
+                                context.add_plan(critical_time, |context|{
+                                    context.set_property(event.entity_id, SymptomStatus::Resolved);
+                                });
+                            }
+                        } else {
+                            let severe_to_resolved = LogNormal::new(severe_to_resolved_mu, severe_to_resolved_sigma).unwrap();
+                            let resolved_time = severe_time + context.sample_distr(SymptomsRng, severe_to_resolved);
+                            context.add_plan(resolved_time, |context|{
+                                context.set_property(event.entity_id, SymptomStatus::Resolved);
+                            });
+                        }
+                    } else {
+                        let mild_to_resolved = LogNormal::new(mild_to_resolved_mu, mild_to_resolved_sigma).unwrap();
+                        let resolved_time = mild_time + context.sample_distr(SymptomsRng, mild_to_resolved);
+                        context.add_plan(resolved_time, |context|{
+                            context.set_property(event.entity_id, SymptomStatus::Resolved);
+                        });
+                    }
+                }
+            }
+        }
+    );
+    Ok(())
+}
+
+// All persons should end up with a SymptomStatus of NoSymptoms, Dead, or Resolved.
+
+// TODO:
+// -set up parameters (essential to make it run)
+// -generate reports about symptom status (both incidence and prevalence are of interest, but could start with incidence)
+// -add SymptomStatusData or some other mechanism to track what most symptom statuses a person had, even after resolved (and maybe track timing, too)
+// -make probabilities age (category) specific
