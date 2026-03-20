9e93598d558d27b41f158971a58e5af8b8035416
Guido Espana <confunguido@gmail.com>
2026-03-20 12:30:20 -0400

    Compare index_setting with just add_setting

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,9 @@
-use epimodel::{ContextParametersExt, Params, initialize_model};
-use ixa::{prelude::*, profiling::ProfilingContextExt, run_with_args};
+//use epimodel::{ContextParametersExt, Params, initialize_model};
+
+use epimodel::model::initialize_model;
+use ixa::{prelude::*};
 
 fn main() {
-    let mut context = run_with_args(|context: &mut Context, args, _| {
-        assert!(
-            args.config.is_some(),
-            "No config file provided, must follow `cargo run -- --config path/to/filename.json`"
-        );
-
-        let &Params { seed, max_time, .. } = context.get_params();
-        initialize_model(context, seed, max_time).expect("Model initialization failed");
-
-        Ok(())
-    })
-    .unwrap();
-
-    // Write the profiling data and context's execution statistics to a JSON file.
-    context.write_profiling_data();
-    ixa::profiling::print_profiling_data();
+    let mut context = Context::new();
+    initialize_model(&mut context, 123, 100.0).expect("Model initialization failed");
 }
