21ff8112c5fef355fbb8ac076d34da49bd63e0f4
Erik Rosenstrom <xui6@cdc.gov>
2026-06-15 13:02:51 -0700

    reworking testing strategy module

--- src/surveillance/test_strategy.rs
+++ src/surveillance/test_strategy.rs
@@ -1,6 +1,5 @@
 use ixa::prelude::*;
 
-use crate::policy::InterventionTrait;
 use crate::population_loader::PersonId;
 use crate::settings::Person;
 use crate::surveillance::{
@@ -16,11 +15,6 @@ pub enum TestStrategy {
     Passive(TestStrategyProperties),
 }
 
-pub trait TestTrait {
-    fn determine_if_testing_occurs(&mut self) -> bool;
-    fn conduct_test(&mut self, context: &mut Context, person_id: PersonId);
-}
-
 #[derive(Debug, Clone, Copy)]
 pub struct TestStrategyProperties {
     pub test_type: TestType,
@@ -29,20 +23,21 @@ pub struct TestStrategyProperties {
     pub post_test_strategy: Option<PostTestStrategy>,
 }
 
-// this test strategy is used when people need to be queried and tested.
-impl TestTrait for TestStrategyProperties {
-    fn determine_if_testing_occurs(&mut self) -> bool {
-        self.testing_adherence > 0.0
+pub trait ContextTestStrategyExt:
+    PluginContext + ContextEntitiesExt + ContextRandomExt + ContextTestExt + ContextPostTestStrategyExt
+{
+    fn determine_if_testing_occurs(&mut self, testing_strategy_properties: TestStrategyProperties) -> bool {
+        self.sample_bool(TestStrategyRng, testing_strategy_properties.testing_adherence)
     }
 
-    // it would be nice to disentangle the testing and post testing triggering
-    fn conduct_test(&mut self, context: &mut Context, person_id: PersonId) {
-        if context.sample_bool(TestStrategyRng, self.testing_adherence) {
+    fn conduct_test(&mut self, person_id: PersonId, test_strategy_properties: TestStrategyProperties) {
+        if self.determine_if_testing_occurs(test_strategy_properties) {
             // Simulate testing delay
-            let test_type = self.test_type;
-            let testing_delay = self.testing_delay;
-            let post_test_strategy = self.post_test_strategy;
-            context.add_plan(context.get_current_time() + testing_delay, move |context| {
+            let test_type = test_strategy_properties.test_type;
+            let testing_delay = test_strategy_properties.testing_delay;
+            let post_test_strategy = test_strategy_properties.post_test_strategy;
+
+            self.add_plan(self.get_current_time() + testing_delay, move |context| {
                 let result = context.test(test_type, person_id);
                 if result && let Some(post_test_strategy) = post_test_strategy {
                     context.post_test_action(person_id, post_test_strategy);
@@ -50,52 +45,45 @@ impl TestTrait for TestStrategyProperties {
             });
         }
     }
-}
 
-impl InterventionTrait for TestStrategy {
-    fn activate<P>(&self, context: &mut Context, group_property: P)
-    where
-        P: Property<Person> + std::fmt::Debug,
-        P::CanonicalValue: std::hash::Hash + Eq + std::fmt::Debug,
+    fn implement_test_strategy<P>(&mut self, group: P, test_strategy: TestStrategy)
+    where        
+    P: Property<Person> + std::fmt::Debug,
+    P::CanonicalValue: std::hash::Hash + Eq + std::fmt::Debug,
     {
-        let strategy = *self;
-        match strategy {
-            TestStrategy::Active(strategy) => {
-                let person_ids: Vec<_> = context
-                    .query_result_iterator(with!(Person, group_property))
-                    .collect();
-                for person_id in person_ids {
-                    context.conduct_test(person_id, strategy);
+        match test_strategy {
+            TestStrategy::Active(props) => self.implement_active_strategy(group, props),
+            TestStrategy::Passive(props) => self.implement_passive_strategy(group, props),
+        }
+    }
+
+    fn implement_active_strategy<P>(&mut self, group: P, test_strategy_properties: TestStrategyProperties)
+    where        
+    P: Property<Person> + std::fmt::Debug,
+    P::CanonicalValue: std::hash::Hash + Eq + std::fmt::Debug,
+    {   
+        let people: Vec<_> = self.query_result_iterator(with!(Person, group)).collect();
+        for person_id in people {
+            self.conduct_test(person_id, test_strategy_properties);
+        }
+    }
+
+    fn implement_passive_strategy<P>(&mut self, group: P, test_strategy_properties: TestStrategyProperties)
+    where        
+    P: Property<Person> + std::fmt::Debug,
+    P::CanonicalValue: std::hash::Hash + Eq + std::fmt::Debug,
+    {
+        self.subscribe_to_event::<PropertyChangeEvent<Person, P>>(
+            move |context, event| {
+                if event.current == group {
+                    context.conduct_test(event.entity_id, test_strategy_properties);
                 }
-            }
-            TestStrategy::Passive(strategy) => {
-                context.subscribe_to_event::<PropertyChangeEvent<Person, P>>(
-                    move |context, event| {
-                        if event.current == group_property {
-                            context.conduct_test(event.entity_id, strategy);
-                        }
-                    },
-                );
-            }
-        }
+            },
+        );
     }
-    fn deactivate<P>(&self, _context: &mut Context, _group_property: P)
-    where
-        P: Property<Person> + std::fmt::Debug,
-        P::CanonicalValue: std::hash::Hash + Eq + std::fmt::Debug,
-    {
-    }
-}
 
-pub trait ContextTestStrategyExt:
-    PluginContext + ContextEntitiesExt + ContextRandomExt + ContextTestExt + ContextPostTestStrategyExt
-{
-    fn conduct_test<T: TestTrait>(&mut self, person_id: PersonId, test_strategy: T);
+
 }
-impl ContextTestStrategyExt for Context {
-    fn conduct_test<T: TestTrait>(&mut self, person_id: PersonId, mut test_strategy: T) {
-        if test_strategy.determine_if_testing_occurs() {
-            test_strategy.conduct_test(self, person_id);
-        }
-    }
-}
+impl ContextTestStrategyExt for Context {}
+
+
